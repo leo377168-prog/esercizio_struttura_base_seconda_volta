ec9f6451a36af283d47d6b912617219c65e6d2d0
leo377168-prog <leo377168@gmail.com>
2026-09-01 20:24:39 +0000

    progetto_finito

--- frontend/src/app/carrello/carrello.component.ts
+++ frontend/src/app/carrello/carrello.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CartService } from '../cart.service'; 
+import { ProdottoModel } from '../models/models';
 
 @Component({
   selector: 'app-carrello',
   standalone: true,
-  imports: [],
-  templateUrl: './carrello.component.html',
-  styleUrl: './carrello.component.css'
+  imports: [RouterLink],
+  templateUrl: './carrello.component.html'
 })
 export class CarrelloComponent {
+  private cartService = inject(CartService);
 
+  // Getter per accedere facilmente all'array del servizio
+  get items(): ProdottoModel[] {
+    return this.cartService.getCart();
+  }
+
+  get totale(): number {
+    return this.cartService.totalPrice();
+  }
+
+  svuota(): void {
+    this.cartService.clearCart();
+  }
 }
